4b9fec5d68ec15400928e24e3b9587376bbde5c7
Parth sharma <109096437+techyperry@users.noreply.github.com>
2023-05-01 20:09:07 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,9 +1,9 @@
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
+import pyttsx3 
+import speech_recognition 
 import datetime
-import wikipedia #pip install wikipedia
+import wikipedia 
 import webbrowser
 import os
-import smtplib
 import pyfiglet
 #you can install modules by running pip install "module name" or py -m pip install "module name" 
+#if you don't know how to install pip then I will make video on "HOW to install PIP" or you can serach on youtube or google
